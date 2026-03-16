0e88cfb4828b2f886ef7aaebcb71de85cacf5d26
Ilvir <ilvirvarvar@mail.ru>
2026-03-16 11:26:08 +0300

    Reset main: added full project from my_balloon_app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_app
+# my_balloon_app
 
 A new Flutter project.
 
--- gas_express_v2/android/app/src/main/kotlin/com/example/gas_express_v2/MainActivity.kt
+++ gas_express_v2/android/app/src/main/kotlin/com/example/gas_express_v2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app
+package com.example.gas_express_v2
 
 import io.flutter.embedding.android.FlutterActivity
 
